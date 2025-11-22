a0b0c0c668702d9a22ddfd87cb91a130a3cf4087
kmanstu <kmanstu11@gmail.com>
2025-11-22 16:13:46 -0700

    Create supabaseClient.js

---
+++ app/lib/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  "https://rckahirpbfcptfontjhh.supabase.co",
+  "sb_publishable_XcChLfne_UUPlNwdlkG98Q_FtD0iuTl"
+);
