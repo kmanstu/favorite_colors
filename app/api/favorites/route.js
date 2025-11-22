c0d31cc70fba2a621a1f0788a75a1516211c8334
kmanstu <kmanstu11@gmail.com>
2025-11-22 16:14:34 -0700

    Create route.js

---
+++ app/api/favorites/route.js
@@ -0,0 +1,19 @@
+import { supabase } from "@/app/lib/supabaseClient";
+
+export async function GET() {
+  const { data, error } = await supabase.from("favorite_colors").select("*");
+  return Response.json({ data, error });
+}
+
+export async function POST(request) {
+  const body = await request.json();
+
+  const { data, error } = await supabase
+    .from("favorite_colors")
+    .insert({
+      name: body.name,
+      color: body.color
+    });
+
+  return Response.json({ data, error });
+}
